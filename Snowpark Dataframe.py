97fcee1961f32b70775f729f97f883878a0030d6
W N <wng1@users.noreply.github.com>
2024-09-27 22:41:22 +0100

    Update Snowpark Dataframe.py

--- Snowpark Dataframe.py
+++ Snowpark Dataframe.py
@@ -4,8 +4,10 @@
 # Session object as a parameter
 # Table method in our session class to generate our dataframe, to connection of Snowflake database, calling existing table in Snowflake
 
+#Create Function to create session object
 def create_dataframe(session):
-  
+
+  #Use variable to define the session's method to retrieve Snowflake API table connectivity to an existing table
   df_table = session.table("SAMPLE_TABLE")
 
 #Actions - show() and collect()
